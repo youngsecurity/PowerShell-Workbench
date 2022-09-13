00e8873e2b7f04ed1548d24046a6282f289a9be4
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2022-09-12 21:22:31 -0400

    +finished Get-PSCore 7 and Preview

--- 03-Start-RemoteDeploymentsWindows.ps1
+++ 03-Start-RemoteDeploymentsWindows.ps1
@@ -5,52 +5,89 @@ Set-Location -Path 'G:\My Drive\!_Work\Notes\PowerShell-Workbench\'
 $HostNames = Get-Content "G:\My Drive\!_Work\Notes\PowerShell-Workbench\hostnames.txt"
 $AdminPrivs = [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
 
+# Future use: If not admin, then whoami and quit if not admin            
+
 Write-Host "Reading hostnames..." -ForegroundColor Red
 try {
     ForEach ($HostName in $HostNames) {
         $PSSession = New-PSSession -ComputerName $HostName #-Credential (Get-Credential)
         Invoke-Command -Session $PSSession -ScriptBlock {
-            #Start-Job -Name PowerShellUpdate -ScriptBlock {
-                hostname
-                Write-Host "Is the script running as Administrator?" -ForegroundColor Red
-                Write-Host "$AdminPrivs"
-                # Future use: If not admin, then whoami and quit if not admin            
-                
+            #Start-Job -Name PowerShellUpdate -ScriptBlock {                
+                Write-Host `r`n
+                Write-Host "hostname:" (hostname)
+                Write-Host "whoami:" (whoami)
+                Write-Host "Is the script running as Administrator?" -ForegroundColor Red                              
                 if ( $AdminPrivs -eq "False") {
-                    Write-Host "This script requires Administrator priviledges and will now exit." -ForegroundColor Red
+                    Write-Host "No, this script requires Administrator priviledges and will now exit." -ForegroundColor Red
                     Write-Host "Pleaase run the script again with Administrator priviledges." -ForegroundColor Red
                     Break
                 }
                 else {
-                    $PSPath = Test-Path -Path "$env:ProgramFiles\PowerShell\7*"
-                    if ($PSPath -eq "True") {
-                        Write-Host "You have PowerShell 7 installed" -ForegroundColor Red
-                        Write-Host "Checking PowerShell version..." -ForegroundColor Red
-                        # Check the installed version of PowerShell
-                        $PSCoreVersion = (Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions\* -Name 'SemanticVersion').SemanticVersion
-                        $PSCoreVersion
+                    Write-Host "Yes, we have Administrator Priviledges"
+                    # Get-PowerShell
+                    Write-Host "Testing PowerShell Core 7 path"
+                    $PSPathCore = Test-Path -Path "$env:ProgramFiles\PowerShell\7"                    
+                    if ($PSPathCore -eq "True") {                        
+                        Write-Host "You have PowerShell 7 Core installed" -ForegroundColor Red
+                        Write-Host "Checking PowerShell 7 Core version..." -ForegroundColor Red
+                        $PSCoreVersion = (Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions\* -Name 'SemanticVersion')#.SemanticVersion                                            
+                        Write-Host "You have PowerShell Core Version(s)" ($PSCoreVersion)
+                    }
+                    else {                                                
+                        Write-Host "You do not have the latest PowerShell Core 7 installed." -ForegroundColor Red                        
                         try {
-                            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                            Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -AddExplorerContextMenu -EnablePSRemoting -Quiet -UseMSI"
+                            winget upgrade
+                            winget source update
+                            winget install Microsoft.PowerShell
+                            #winget install Microsoft.PowerShell.Preview
+                            #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                            #Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -AddExplorerContextMenu -EnablePSRemoting -Quiet -UseMSI"                            
                         }
                         catch {
                             Write-Host "An Error Occured" -ForegroundColor Red
                             Write-Host $PSItem.Exception.Message -ForegroundColor Red
                         }
                         finally {
-                            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                            Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -AddExplorerContextMenu -EnablePSRemoting -Preview -Quiet -UseMSI"
+                            $PSCoreVersion = (Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions\* -Name 'SemanticVersion')                            
+                            Write-Host "Finished Installing PowerShell 7 Core"
+                        }                    
+                    }
+                    $PSPathCorePreview = Test-Path -Path "$env:ProgramFiles\PowerShell\7-preview"       
+                    if ($PSPathCorePreview -eq "True") {
+                        Write-Host "You have PowerShell 7 Core Preview installed" -ForegroundColor Red
+                        Write-Host "Checking PowerShell 7 Core Preview version..." -ForegroundColor Red
+                        $PSCoreVersion = (Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions\* -Name 'SemanticVersion')
+                        Write-Host "You have PowerShell 7 Core Preview Version" ($PSCoreVersion[1])
+                        if ($PSCoreVersion[1] -ne "7.3.0-preview.7") {
+                            Write-Host "You do not have the latest PowerShell Core 7 Preview Version installed. Your version is:" $PSCoreVersion[1]                            
+                            Write-Host "Installing the latest PowerShell Core 7 Preview"
+                            winget upgrade
+                            winget source update
+                            winget install Microsoft.PowerShell.Preview
+                        }
+                        else {
+                            Write-Host "You have the latest PowerShell Core 7 Preview"
                         }
                     }
                     else {
-                        Write-Host "$PSCoreVersion"
-                        Write-Host "You do not have the latest PowerShell Core 7 Preview installed." -ForegroundColor Red
-                        Write-Host "Installing PowerShell Core 7 Preview..." -ForegroundColor Red                        
-                        #winget install Microsoft.PowerShell
-                        #winget install Microsoft.PowerShell.Preview
-                    }                                        
-                #}
-            }        
+                        Write-Host "You do not have the latest PowerShell Core 7 Preview installed." -ForegroundColor Red                        
+                        try {                            
+                            winget install Microsoft.PowerShell.Preview
+                            #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                            #Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -AddExplorerContextMenu -EnablePSRemoting -Quiet -UseMSI"                            
+                        }
+                        catch {
+                            Write-Host "An Error Occured" -ForegroundColor Red
+                            Write-Host $PSItem.Exception.Message -ForegroundColor Red
+                        }
+                        finally {
+                            #$PSCoreVersion = (Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Microsoft\PowerShellCore\InstalledVersions\* -Name 'SemanticVersion')#.SemanticVersion                    
+                            Write-Host "You should now have both PowerShell Core Versions" $PSCoreVersion
+                            Write-Host "Finished Installing PowerShell 7 Core"
+                        }                    
+                    }                    
+                }                
+            #}    
         } -ErrorAction Continue      
     }
 }
@@ -67,4 +104,5 @@ finally {
     $Error.Clear()
     Remove-PSSession -Session $PSSession
 }
+
 
