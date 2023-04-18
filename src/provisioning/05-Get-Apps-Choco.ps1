421edaf9b1aea85c96fdfb7838e543544210eb21
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-04-18 15:54:18 -0400

    mod 05-get-Apps-Choco.ps1

--- src/provisioning/05-Get-Apps-Choco.ps1
+++ src/provisioning/05-Get-Apps-Choco.ps1
@@ -1,2 +1,10 @@
 # After choco is done installing, add more apps
 choco install -y chocolateygui googlechrome lastpass grammarly winrar python3 treesizefree
+
+# Install vscode in a custom directory
+# TODO: Prompt the user for the path to install vscode
+choco install vscode.install -y --install-arguments="'/DIR=""F:\Program Files\Microsoft VS Code""'" --force
+
+# Install vscode-insiders in a custom directory
+# TODO: Prompt the user for the path to install vscode-insiders
+choco install vscode-insiders.install -y --install-arguments="'/DIR=""F:\Program Files\Microsoft VS Code Insiders""'" --force
