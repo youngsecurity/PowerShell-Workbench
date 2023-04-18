9c4273b0a7a9f842c49dae59f3bf3db71d635ad8
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-04-18 15:50:21 -0400

    +mod get apps and upgrade apps scripts

---
+++ src/maintenance/Choco-Upgrade-Apps.ps1
@@ -0,0 +1,2 @@
+# Command to upgrade vscode and vscode-insiders
+choco upgrade vscode.install -y && choco upgrade vscode-insiders.install -y
---
+++ src/provisioning/05-Get-Apps-Choco.ps1
@@ -0,0 +1,2 @@
+# After choco is done installing, add more apps
+choco install -y chocolateygui googlechrome lastpass grammarly winrar python3 treesizefree
