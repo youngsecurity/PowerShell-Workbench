9765db91b003ff361d9447b72699d3dbbed739a0
Joseph Young <3170654+redlinejoes@users.noreply.github.com>
2023-05-08 02:10:25 -0400

    +update metadata

--- src/provisioning/01-Set-LocalhostSecurity.ps1
+++ src/provisioning/01-Set-LocalhostSecurity.ps1
@@ -1,7 +1,24 @@
-# For non-AD workgroup systems only; Steps required to run locally in order to enable PSRemoting
-# Get Windows Network Connection Profile and set them all to Private
-# Set Windows Adv Firewall to enable File and Print Sharing on Private zone
-# Set Windows Admin Shares required for Workgroup computers only
+<#
+.NOTES
+    Script Name: 01-Set-LocalhostSecurity.ps1
+    Author: Joseph Young <joe@youngsecurity.net>
+    Date: 5/8/26/2023
+    Copyright: (c) Young Security Inc.
+    Licensed under the MIT License.
+
+.SYNOPSIS
+    For non-AD workgroup systems only; Steps required to run locally in order to enable PSRemoting
+
+.DESCRIPTION
+    Get Windows Network Connection Profile and set them all to Private
+    Set Windows Adv Firewall to enable File and Print Sharing on Private zone, which enables ICMP echo
+    Set Windows Admin Shares required for Workgroup computers only, required for WinRM
+
+.EXAMPLE
+    .\01-Set-LocalhostSecurity.ps1 <arguments>    
+#>
+
+
 Try {
     Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
     Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True -Profile Private
